22afab2706d3bad46c233889652b2269749550c5
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-03-21 18:58:45 +0530

    converted the jsx code in DateCounter to tsx

--- src/DateCounter.tsx
+++ src/DateCounter.tsx
@@ -1,9 +1,19 @@
-import { useReducer } from "react";
+import React, { useReducer } from "react";
 
-const initialState = { count: 0, step: 1 }
+interface State {
+  count: number;
+  step: number;
+}
+
+interface Action {
+  type: string;
+  payload?: number;
+}
+
+const initialState : State = { count: 0, step: 1 };
 
 // reducer has 2 arguements -> current state and the action
-function reducer(state, action){
+function reducer(state : State, action : Action): State{
   console.log(state, action);
   // We could also change the current state by creating a new object by destructuring and spreading the previous state and overwritting the current state
   switch(action.type){
@@ -12,9 +22,9 @@ function reducer(state, action){
     case "dec":
       return {...state, count: state.count - state.step}
     case "setCount":
-      return {...state, count: action.payload }
+      return {...state, count: action.payload !== undefined? action.payload : 0}
     case "setStep":
-      return {...state, step: action.payload }
+      return {...state, step: action.payload !== undefined? action.payload : 0 }
     case "reset":
       return initialState
     default:
@@ -26,7 +36,7 @@ function reducer(state, action){
 // When does this reducer function get called?
 // -> The reducer function gets called when you dispatch an action, where action is basically an object containing all the state pieces.
 
-function DateCounter() {
+function DateCounter(): JSX.Element {
   const [state, dispatch] = useReducer(reducer, initialState);
   const {count, step} = state;
   // const [state-object, dispatch  is a function] = useReducer(reducer function, initial state value);
@@ -47,12 +57,12 @@ function DateCounter() {
     // dispatch(1);  the value of action is what we pass into the dispatch function
   };
 
-  const defineCount = function (e) {
+  const defineCount = function (e : React.ChangeEvent<HTMLInputElement>): void{
     dispatch({ type: "setCount", payload: Number(e.target.value)})
     // setCount(Number(e.target.value));
   };
 
-  const defineStep = function (e) {
+  const defineStep = function (e: React.ChangeEvent<HTMLInputElement>): void{
     dispatch({ type: "setStep", payload: Number(e.target.value)})
     // setStep(Number(e.target.value));
   };
