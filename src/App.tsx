f0ef5ce20349a6367d0f8ecdd51099ff6a3ef09b
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-03-24 23:33:20 +0530

    starting a new quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useReducer } from "react"
 import Body from "./Body"
-import  DateCounter  from "./DateCounter"
 import Header from "./Header"
 import Loader from "./Loader"
 import Error from "./Error"
 import StartScreen from "./StartScreen"
+import Question from "./Question"
 
 // 1.We have to grab this URL and then fetch it into our application. // We want to load the data on mount and for that we need to use useEffect hook
 // 2. At some point we will have to display the fetched data here in the UI and so for that we are going to need state which we will get using the useReducer hook
@@ -29,12 +29,17 @@ function reducer(state, action){
         ...state,
         status: "error"
       };
+    case "state":
+      return {
+        ...state,
+        status: "active"
+      }
     default: 
       throw new Error("Unknown Action")
   }
 }
 
-export default function App({children}){
+export default function App(){
   const [{quesions, status}, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = quesions.length;
@@ -52,9 +57,11 @@ export default function App({children}){
     <div className="app">
       <Header />
       <Body >  
-        {status === 'loading' && <Loading />}
+        {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions}/>}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
+        {/* Now how do I set the status to active. Ans is by creating another action in the switch case and then returning the new object with the status set to active */}
+        {status === 'active' && <Question />}
       </Body>
     </div>
   )
--- src/StartScreen.tsx
+++ src/StartScreen.tsx
@@ -1,11 +1,11 @@
 
 
-export default function StartScreen({ numQuestions }) {
+export default function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
         <h2>Welcome to the REACT Quiz!</h2>
         <h3>{numQuestions} questions to test your REACT mastery</h3>
-        <button className="btn btn-ui">Let's Start</button>
+        <button className="btn btn-ui" onClick={() => dispatch({ type: "start" })}>Let's Start</button>
     </div>
   )
 }
